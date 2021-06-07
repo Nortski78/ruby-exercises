5d218c63143733d8e5e0912d3f399ea5648b5eea
Mark McNaughton <markusmack@hotmail.com>
2021-06-07 12:59:18 +0100

    Fixed shifting and non alphabet characters

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,12 +1,35 @@
 def caesar(string, shift)
-    string = string.bytes
+    
+    # Convert to ascii characters and return as an array
+    string = string.bytes    
+    
+    # Process each element in the string array
     string.map! do | ascii |
-      if (ascii.between?(97, 122))
-        (ascii - shift) < 97 ? ascii - shift + 26 : ascii - shift
+        # If lower case, process the shift including wrapping
+        if (ascii.between?(97, 122))
+            if (shift < 0)
+                (ascii - shift) < 97 ? ascii - shift + 26 : ascii - shift
+            elsif (shift >= 0)
+                (ascii + shift) > 122 ? ascii + shift - 26 : ascii + shift
+            end
+        # If upper case, process the shift including wrapping
         elsif (ascii.between?(65, 90))
-          (ascii - shift) < 65 ? ascii - shift + 26 : ascii - shift
-      end
+            if (shift < 0)
+                (ascii - shift) < 65 ? ascii - shift + 26 : ascii - shift
+            elsif (shift >= 0)
+                (ascii + shift) > 90 ? ascii + shift - 26 : ascii + shift
+            end
+        else
+            # Leave original character as it was
+            ascii
+        end
     end
+    
+    # Convert back to a string
     string = string.pack('c*')
+    
     p string
- end
+end
+
+caesar("What a string!", 5)
+
